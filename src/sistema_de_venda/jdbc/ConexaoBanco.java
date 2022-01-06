28eb4efffa9577f35d7cd12312f2bfb0bcb42fb9
LeandroCostaDomingues <leandrocosta190991@gmail.com>
2022-01-05 21:03:47 -0300

    Projeto Criado

---
+++ src/sistema_de_venda/jdbc/ConexaoBanco.java
@@ -0,0 +1,18 @@
+package sistema_de_venda.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.swing.JOptionPane;
+
+public class ConexaoBanco {
+	public Connection conexao() {
+		try {
+			return DriverManager.getConnection("jdbc:mysql://localhost/sistemasdevendas", "root",null);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, " Erro De Conexão " + e);
+
+		}
+		return null;
+	}
+}
---
+++ src/sistema_de_venda/jdbc/Programa.java
@@ -0,0 +1,19 @@
+package sistema_de_venda.jdbc;
+
+import javax.swing.JOptionPane;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		try {
+			new ConexaoBanco().conexao();
+			JOptionPane.showInternalMessageDialog(null, "Conectado");
+
+		} catch (Exception e) {
+			JOptionPane.showInternalMessageDialog(null, "Erro De Conexão"+e.getStackTrace());
+			
+		}
+
+	}
+
+}
